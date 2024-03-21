0def1517ac13f2cad63da4331d5e7d4b1ed3c78b
MD ZAKIR HOSSAIN <159409026+zakirSIR@users.noreply.github.com>
2024-03-21 12:59:43 +0600

    Add files via upload
    I have complete the project-1

---
+++ javascript.js
@@ -0,0 +1,15 @@
+//navigation bar এর কাজ
+/*
+let menuList= document.querySelector(".menuList")
+menuList.style.maxHeight="0px";
+
+function toggleMenu(){
+	if(menuList.style.maxHeight=="0px"){
+		menuList.style.maxHeight="300px"
+	}
+	else{
+		menuList.style.maxHeight="0px"
+	}
+}
+*/
+
